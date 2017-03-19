095c19da483203f4757b86cb18364ed3de053d60
gbvivian <gbvivian@hotmail.com>
2017-03-19 12:41:57 -0700

    cleaned code/delete unused classes

--- app/src/main/java/com/example/kristen/goldfish/MainActivity.java
+++ app/src/main/java/com/example/kristen/goldfish/MainActivity.java
@@ -35,88 +35,6 @@ public class MainActivity extends AppCompatActivity {
         v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         EditText editText = (EditText) findViewById(R.id.itemName);
         editText.setBackgroundColor(Color.WHITE);
-
-       // mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-//        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_SIGNIFICANT_MOTION);
-//
-//        mTriggerEventListener = new TriggerEventListener() {
-//            @Override
-//            public void onTrigger(TriggerEvent event) {
-//                Toast.makeText(getApplicationContext(), "PINIA IS THE BEST", Toast.LENGTH_SHORT).show();
-//            }
-//        };
-
-
-
-//
-//        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-//        mSensorEventListener = new SensorEventListener() {
-//            @Override
-//
-//            public void onSensorChanged(SensorEvent sensorEvent) {
-//                if (sensorEvent.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)
-//                {
-//                    return;
-//                }
-//
-//                if (sensorEvent.values[0] > 3.5 || sensorEvent.values[1] > 3.5 || sensorEvent.values[2] > 3.5 || sensorEvent.values[0] < -3.5 || sensorEvent.values[1] < -3.5 || sensorEvent.values[2] < -3.5) {
-//                    Toast.makeText(getApplicationContext(), "PINIA IS THE BEST", Toast.LENGTH_SHORT).show();
-//                    Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-//                    v.vibrate(500);
-//                }
-//            }
-//
-//            public void onAccuracyChanged(Sensor sensor, int i) {
-//
-//            }
-//        };
-
-
-        /*
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        mSensorEventListener = new SensorEventListener() {
-            public void onSensorChanged(SensorEvent event) {
-                // alpha is calculated as t / (t + dT)
-                // with t, the low-pass filter's time-constant
-                // and dT, the event delivery rate
-
-                final float alpha = 0.8;
-               // final float alpha = 0.8;
-
-                double[] gravity = new double[3];
-                gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
-                gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
-                gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
-
-                double[] linear_acceleration = new float[3];
-                linear_acceleration[0] = event.values[0] - gravity[0];
-                linear_acceleration[1] = event.values[1] - gravity[1];
-                linear_acceleration[2] = event.values[2] - gravity[2];
-            }
-
-            public void onAccuracyChanged(Sensor sensor, int i) {
-
-float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
-
-            if (speed > SHAKE_THRESHOLD) {
-                getRandomNumber();
-            }
-            }
-
-        };
-        */
-
-
-
-
-
-
-
-       // mSensorManager.registerListener(mSensorEventListener, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-       // mSensorManager.requestTriggerSensor(mTriggerEventListener, mSensor);
-
-
     }
 
     public void setReminder(View view) {
@@ -124,47 +42,30 @@ float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
         itemName = itemNameEdit.getText().toString();
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
         mSensorEventListener = new SensorEventListener() {
             @Override
 
             public void onSensorChanged(SensorEvent sensorEvent) {
-                if (sensorEvent.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)
-                {
-                    return;
-                }
+                if (sensorEvent.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) {return;}
 
-                if (sensorEvent.values[0] > 3.5 || sensorEvent.values[1] > 3.5 || sensorEvent.values[2] > 3.5 || sensorEvent.values[0] < -3.5 || sensorEvent.values[1] < -3.5 || sensorEvent.values[2] < -3.5) {
+                if (sensorEvent.values[0] > 3 || sensorEvent.values[1] > 3 || sensorEvent.values[2] > 3 || sensorEvent.values[0] < -3 || sensorEvent.values[1] < -3 || sensorEvent.values[2] < -3) {
                     Toast.makeText(getApplicationContext(), "Don't forget to " + itemName, Toast.LENGTH_SHORT).show();
                     v.vibrate(500);
 
-                    Notification.notificatePush(getBaseContext(), 1, "goldfish", "Don't forget to..." + itemName, getIntent(), getWindow());
-
-
+                    Notification.notificatePush(getBaseContext(), 1, "goldfish", "Don't forget to " + itemName, getIntent(), getWindow());
                 }
             }
 
             @Override
             public void onAccuracyChanged(Sensor sensor, int i) {
-
             }
         };
 
         mSensorManager.registerListener(mSensorEventListener, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        // mSensorManager.requestTriggerSensor(mTriggerEventListener, mSensor);
-
-
         Toast.makeText(getApplicationContext(), "We gotchu", Toast.LENGTH_SHORT).show();
         v.vibrate(100);
-//        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.editText);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
     }
-
 
 }
--- app/src/main/java/com/example/kristen/goldfish/Notification.java
+++ app/src/main/java/com/example/kristen/goldfish/Notification.java
@@ -39,101 +39,35 @@ public class Notification {
                     .setContentTitle(contentTitle)
                     .setContentText(contentText)
                     .setVisibility(visibility);
-                  //  .setTicker(tickerText);
 
-            // Because clicking the notification opens a new ("special") activity, there's no need to create an artificial back stack.
-            // took resultPending intent
             PendingIntent resultPendingIntent = PendingIntent.getActivity(context, notificationId, intent, PendingIntent.FLAG_ONE_SHOT);
             mBuilder.setContentIntent(resultPendingIntent);
             mBuilder.setAutoCancel(true);
-           // mBuilder.setOngoing(true);
             mBuilder.setOnlyAlertOnce(true);
 
         Bitmap notificationLargeIconBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.almost_new_goldfish);
         mBuilder.setLargeIcon(notificationLargeIconBitmap);
 
         PendingIntent dismissIntent = NotificationActivity.getDismissIntent(notificationId, context);
-        mBuilder.addAction(R.drawable.dinia, "dismiss", dismissIntent);
+        mBuilder.addAction(R.drawable.denia, "dismiss", dismissIntent);
 
-      //  Intent triggerIntent = new Intent(getApplicationContext(), ActivityToLaunch.class);
-     //   triggerIntent.putExtra(Intent.EXTRA_KEY_EVENT, dism);
+        // Gets an instance of the NotificationManager service
+        notifyMgr = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        // Builds the notification and issues it.
+        notifyMgr.notify(notificationId, mBuilder.build());
 
-            // Gets an instance of the NotificationManager service
-            notifyMgr = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        // V ADDED START
+        PowerManager pm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
+        boolean isScreenOn = pm.isScreenOn();
+        Log.e("screen on.............", ""+isScreenOn);
+        if(isScreenOn==false)
+        {PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK |PowerManager.ACQUIRE_CAUSES_WAKEUP |PowerManager.ON_AFTER_RELEASE,"MyLock");
+            wl.acquire(10000);
+            PowerManager.WakeLock wl_cpu = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"MyCpuLock");
 
-            // Builds the notification and issues it.
-            notifyMgr.notify(notificationId, mBuilder.build());
-
-            PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-//            PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
-//                    "MyWakelockTag");
-//            wakeLock.acquire();
-//
-//            PowerManager.WakeLock screenLock = ((PowerManager)getSystemService(POWER_SERVICE)).newWakeLock(
-//                    PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
-//            screenLock.acquire();
-
-            // V ADDED START
-            PowerManager pm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
-            boolean isScreenOn = pm.isScreenOn();
-            Log.e("screen on.............", ""+isScreenOn);
-            if(isScreenOn==false)
-            {
-                PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK |PowerManager.ACQUIRE_CAUSES_WAKEUP |PowerManager.ON_AFTER_RELEASE,"MyLock");
-                wl.acquire(10000);
-                PowerManager.WakeLock wl_cpu = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"MyCpuLock");
-
-                wl_cpu.acquire(10000);
-            }
-            // V ADDED END
-
-            // V COMMENTED OUT BEGINNING
-            /*
-            window.addFlags( WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-                    WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
-                  //  WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON );
-            */
-            // V COMMENTED OUT END
-//           window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-
-
-
-
-        }
-        public void dismiss() {
-          //notifyMgr.cancel(1);
-            notifyMgr.cancelAll();
+            wl_cpu.acquire(10000);
         }
 
-    /*
-    NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
-                    .setSmallIcon(R.drawable.dinia)
-                    .setContentTitle("goldfish")
-                    .setContentText("Hello World!");
-    // Creates an explicit intent for an Activity in your app
-    Intent resultIntent = new Intent(this, ResultActivity.class);
-
-    // The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
-    TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-// Adds the back stack for the Intent (but not the Intent itself)
-stackBuilder.addParentStack(ResultActivity.class);
-// Adds the Intent that starts the Activity to the top of the stack
-stackBuilder.addNextIntent(resultIntent);
-    PendingIntent resultPendingIntent =
-            stackBuilder.getPendingIntent(
-                    0,
-                    PendingIntent.FLAG_UPDATE_CURRENT
-            );
-mBuilder.setContentIntent(resultPendingIntent);
-    NotificationManager mNotificationManager =
-            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on.
-mNotificationManager.notify(mId, mBuilder.build());
-*/
-
+        }
 }
